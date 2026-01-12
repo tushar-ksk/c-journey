b12355b673b6e36db1d45acb0d94fc50ec14ba90
Tushar <tusharsharma1328@gmail.com>
2026-01-12 19:49:04 +0530

    Fix word count logic in 01_word_count.c

--- 03_C_homework/01_String/practice/01_word_count.c
+++ 03_C_homework/01_String/practice/01_word_count.c
@@ -10,7 +10,7 @@ int main(){
     }
     while (str[i] != '\0')
     {
-        if (str[i] != ' ' && str[i-1] == ' ' && str[i+1] != '\0')
+        if (str[i] != ' ' && str[i-1] == ' ')
         {
             count++;
         }
